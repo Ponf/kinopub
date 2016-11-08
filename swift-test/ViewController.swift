3344cc3803ec1917290d595b704924e5597831bc
Filipp <ponf@inbox.ru>
2016-11-08 14:34:42 +0300

    Initial Commit

---
+++ swift-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-test
+//
+//  Created by Филипп Панфилов on 08/11/16.
+//  Copyright © 2016 babystep.tv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
